4385a842266f2d8be36ebc23f090fe4dfa0b51b7
vladimirmenezes <vladimir.menezes@ba.estudante.senai.br>
2026-02-26 20:40:16 -0300

    terceiro commit

--- switch_case.js
+++ switch_case.js
@@ -1,9 +1,21 @@
 const opção = 3
-switch (key) {
-    case value:
-        
+switch (1) {
+    case 1:
+        console.log("opção1")
         break;
-
+    case 2:
+        console.log("opção2")
+        break;
+    case 3:
+        console.log("opção3")
+        break;
+    case 4:
+        console.log("opção4")
+        break;
+    case 5:
+        console.log("opção5")
+        break;
     default:
+        console.log("opçao invalida")
         break;
 }
