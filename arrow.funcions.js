13f3fa73d0dfdeae914510d0ad2c2987e308ee52
vladimirmenezes <vladimir.menezes@ba.estudante.senai.br>
2026-02-27 19:15:47 -0300

    função

---
+++ arrow.funcions.js
@@ -0,0 +1,14 @@
+const somar = (a,b) =>{
+    return a + b
+}
+const subtrair = (a,b)=>a - b;
+
+//CHAMADA DA FUNÇÃp
+const soma = (2,3)
+const  subtraçao = subtrair (2,3)
+
+
+//EXIBINDO RESULTADO
+console.log (`soma:${soma}`)
+console.log (`Subtração:${subtraçao}`)
+
