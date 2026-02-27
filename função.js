6eb01b9196b5720df87be732ec80ed2ffa357635
vladimirmenezes <vladimir.menezes@ba.estudante.senai.br>
2026-02-26 21:39:06 -0300

    quinto commit

---
+++ função.js
@@ -0,0 +1,25 @@
+function somar(a,b) {
+    return a+b
+}
+const soma = somar (2,3)
+console.log (`soma:${soma}`)
+
+function subtrair(a,b) {
+    return a-b
+}
+const sub = subtrair (2,3)
+console.log (`Subitração:${sub}`)
+
+function mult(a,b) {
+    return a*b
+}
+const mul = mult (2,3)
+console.log (`Multiplicação:${mul}`)
+
+function divisao(a,b) {
+    return a/b
+}
+const div = divisao (2,3)
+console.log (`Divisão:${div}`)
+
+
