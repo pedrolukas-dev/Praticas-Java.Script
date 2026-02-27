11f5d7f084d465e88490e4e3f8b04383c3186910
vladimirmenezes <vladimir.menezes@ba.estudante.senai.br>
2026-02-27 19:46:58 -0300

    vetores

---
+++ vetores.js
@@ -0,0 +1,34 @@
+
+//Criando um vetor
+
+const listaNomes =["marta","julia","Ana"]
+console.log("\nExibindo todos os Nomes:\n")
+console.log(listaNomes)
+
+console.log("\nPrimerio Nome:\n")
+console.log(listaNomes [0])
+
+console.log("\nSegudno Nome:\n")
+console.log(listaNomes [1])
+
+console.log("\nTerceiro Nome:\n")
+console.log(listaNomes [3])
+
+console.log("\nAdicionar um novo nome\n")
+listaNomes.push("mariana")
+console.log(listaNomes)
+
+console.log("\nRemover O Primeiro nome\n")
+listaNomes.shift()
+console.log(listaNomes)
+
+console.log("\nRemover O Ultimo nome \n")
+listaNomes.pop()
+console.log(listaNomes)
+console.log("\nRemover Um elemento em Especifico\n")
+listaNomes.split(1,1)
+console.log(listaNomes)
+
+
+
+
