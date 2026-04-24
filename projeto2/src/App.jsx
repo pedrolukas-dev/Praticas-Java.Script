c188f4b27c4611f0377261dc6c0bb2762c19f3fb
Pedro Lucas <pedro.l.santos14@ba.estudante.senai.br>
2026-04-23 21:15:49 -0300

    projeto Tio patinhas

--- projeto2/src/App.jsx
+++ projeto2/src/App.jsx
@@ -1,13 +1,28 @@
-
+import { Route,Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import FaleConosco from './pages/FaleConosco'
+import Home from './pages/Home'
+import SobreNos from './pages/SobreNos'
+import { useState } from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>
-      </h1>
+      <Header>
+        <main>
+          {/* QUalquer página terar header e footer */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre-nos" element={<SobreNos />} />
+          <Route path="/fale-conosco" element={<FaleConosco />} />
+        </Routes>
+        </main>
+        <Footer/>
+      </Header>
     </>
   )
 }
--- projeto2/src/components/Header/index.jsx
+++ projeto2/src/components/Header/index.jsx
@@ -1,21 +1,23 @@
-import './style.css'
-import {Link,Navlink} from 'react-router-dom'
-import logo from '../../assets/logo.png'
-
+import './style.css';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 export default function Header() {
-    return (
-        <header>
-            <div>
-                <link to="/">
-                <img src={logo} alt="Logo da Empresa"/>
-                <span>Contabilidade Simples</span>
-            </link>
-            <nav>
-                <NaveLink to= "/sobre-nos"> Sobre nós</NaveLink>
-                <NavLink to= "/fale-conosco"> Fale conosco</NavLink>
-            </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header className="senai-header">
+      <div className="header-container">
+        
+        <Link to="/" className="logo-container">
+          <img src={logo} alt="Logo da Empresa" />
+          <span>Contabilidade Simples</span>
+        </Link>
+
+        <nav className="nav-menu">
+          <NavLink to="/sobre-nos">Sobre nós</NavLink>
+          <NavLink to="/fale-conosco">Fale conosco</NavLink>
+        </nav>
+
+      </div>
+    </header>
+  );
 }
